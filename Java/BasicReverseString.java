7f5a0136e85ddab0b750fe7174e3748121fe6a32
Tanawat Boonmak <32984208+lattebbrook@users.noreply.github.com>
2022-12-14 22:36:54 +0700

    Update BasicReverseString.java

--- Java/BasicReverseString.java
+++ Java/BasicReverseString.java
@@ -10,6 +10,15 @@ public class BasicReverseString {
         // Use new StringBuilder to shorten the syntax or alternatively use for loop.
         String reversed = new StringBuilder(userInput).reverse().toString();
         System.out.println(reversed);
+        
+        /* 
+        String reversed = "";
+        for (int i = userInput.length() - 1; i >= 0; i--) {
+        reversed += userInput.charAt(i);
+        }
+        System.out.println(reversed);
+        
+        */
 
     }
 }
